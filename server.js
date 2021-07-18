b249973ce74330c3777cbd28ddb068c3618e10d2
lovishprajapati <prajapatilovish@gmail.com>
2021-07-18 21:47:16 +0530

    pf

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const path = require('path')
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
