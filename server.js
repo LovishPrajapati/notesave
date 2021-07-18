0ddb066dd0a4c11299a82de71f5e9e68a9f2d413
lovishprajapati <prajapatilovish@gmail.com>
2021-07-18 21:04:12 +0530

    fixed package

--- server.js
+++ server.js
@@ -31,11 +31,15 @@ app.use("/api", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/user/:id/note", noteRoutes);
 
-app.use(express.static(path.join(__dirname, '../build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'))
-})
-
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("frontend/build"));
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(
+			path.resolve(__dirname, "frontend", "build", "index.html")
+		);
+	});
+}
 // app.listen(PORT, () => {
 // 	console.log(`Server up and running on port ${PORT}`);
 // });
